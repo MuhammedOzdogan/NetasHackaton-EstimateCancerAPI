806a5b0dbfbd078f5ce7d9dc64cc69a0315655c5
Muhammed ÖZDOĞAN <muhammed.ozdogan2@ogr.sakarya.edu.tr>
2020-12-24 00:31:51 +0300

    Fix requirements.txt

--- app.py
+++ app.py
@@ -9,13 +9,27 @@ model = ml.train_model()
 
 
 @app.route('/')
-def hello_world():
+def main():
+    if request.args.__len__() == 0:
+        return app.send_static_file("error.html")
+    if not is_args_valid():
+        return "Apart from age attribute feature values must be between 1 and 10"
+
     x = read_feature_matrix_from_query_params()
     result = model.predict(x)
 
     return {
         "cancer_predict": result[0]
     }
+
+
+def is_args_valid():
+    for key in request.args:
+        v = int(request.args.get(key))
+        if  key != "age" and v > 10 or v < 1:
+            return False
+
+    return True
 
 
 def read_feature_matrix_from_query_params() -> np.array:
@@ -45,6 +59,7 @@ def read_feature_matrix_from_query_params() -> np.array:
 
 def arg(key) -> int:
     return int(request.args.get(key))
+
 
 if __name__ == '__main__':
     app.run()
